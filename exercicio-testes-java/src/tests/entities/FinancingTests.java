2deebb3a9e8de01de5f9ec942969858589415d71
BrunoSola <brunosola3@gmail.com>
2024-06-30 16:17:13 -0300

    Teste Construtor

---
+++ exercicio-testes-java/src/tests/entities/FinancingTests.java
@@ -0,0 +1,25 @@
+package tests.entities;
+
+import entities.Financing;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tests.factory.FinancingFactory;
+
+public class FinancingTests {
+
+    @Test
+    public void constructorShouldCreateObjectWhenValidData(){
+        Financing financing = FinancingFactory.createFinancing();
+
+        Assertions.assertEquals(100000.0, financing.getTotalAmount());
+        Assertions.assertEquals(2000.0, financing.getIncome());
+        Assertions.assertEquals(80, financing.getMonths());
+    }
+
+    @Test
+    public void constructorShouldThrowIllegalArgumentExceptionWhenInvalidData(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing financing = new Financing(100000.0, 2000.0,20);
+        });
+    }
+}
---
+++ exercicio-testes-java/src/tests/factory/FinancingFactory.java
@@ -0,0 +1,10 @@
+package tests.factory;
+
+import entities.Financing;
+
+public class FinancingFactory {
+
+    public static Financing createFinancing(){
+        return new Financing(100000.0, 2000.0,80);
+    }
+}
