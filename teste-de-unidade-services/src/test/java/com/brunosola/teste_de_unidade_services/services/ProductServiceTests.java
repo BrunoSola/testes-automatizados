b2c465ecc5f1c9f6f8fdd554e7617bf6632b0af8
BrunoSola <brunosola3@gmail.com>
2024-07-09 19:31:29 -0300

    :white_check_mark: test: Teste do método findAll

--- teste-de-unidade-services/src/test/java/com/brunosola/teste_de_unidade_services/services/ProductServiceTests.java
+++ teste-de-unidade-services/src/test/java/com/brunosola/teste_de_unidade_services/services/ProductServiceTests.java
@@ -11,8 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -26,17 +31,21 @@ public class ProductServiceTests {
     @Mock
     private ProductRepository productRepository;
 
+    private PageImpl<Product> productPage;
     private Product product;
     private ProductDTO productDTO;
+
 
     @BeforeEach
     void setUp(){
         product = Factory.createProduct();
         productDTO = Factory.createProductDTO();
+        productPage = new PageImpl<>(List.of(product));
 
         doThrow(ResourceNotFoundException.class).when(productRepository).findById(2L);
 
         when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        when(productRepository.findAll((Pageable) any())).thenReturn(productPage);
     }
 
     @Test
@@ -56,5 +65,14 @@ public class ProductServiceTests {
         Assertions.assertThrows(ResourceNotFoundException.class,
                 () -> productService.findById(2L));
         verify(productRepository).findById(2L);
+    }
+
+    @Test
+    public void findAllShouldReturnAllProductsPaged(){
+        Pageable pageable = PageRequest.of(1, 10);
+        Page<ProductDTO> products = productService.findAll(pageable);
+
+        Assertions.assertNotNull(products);
+        verify(productRepository).findAll(pageable);
     }
 }
