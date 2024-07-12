ec8208b044684eba58a1193365a70e4ede0fccca
BrunoSola <brunosola3@gmail.com>
2024-07-12 03:40:09 -0300

    init: commit inicial

---
+++ teste-de-integracao/src/main/java/com/brunosola/teste_de_integracao/TesteDeIntegracaoApplication.java
@@ -0,0 +1,13 @@
+package com.brunosola.teste_de_integracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDeIntegracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDeIntegracaoApplication.class, args);
+	}
+
+}
---
+++ teste-de-integracao/src/test/java/com/brunosola/teste_de_integracao/TesteDeIntegracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosola.teste_de_integracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDeIntegracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
