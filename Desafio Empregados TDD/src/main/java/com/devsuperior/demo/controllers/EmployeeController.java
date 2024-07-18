60a83805607171ee27e8f6227be918c00bda7eab
BrunoSola <brunosola3@gmail.com>
2024-07-18 04:46:11 -0300

    feat: Implementa código conforme TDD EmployeeControllerIT

---
+++ Desafio Empregados TDD/src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.EmployeeDTO;
+import com.devsuperior.demo.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<Page<EmployeeDTO>> findAllPaged(Pageable pageable) {
+        return ResponseEntity.ok(employeeService.findAllPaged(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO employeeDTO) {
+        employeeDTO = employeeService.insert(employeeDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(employeeDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(employeeDTO);
+    }
+}
---
+++ Desafio Empregados TDD/src/main/java/com/devsuperior/demo/services/EmployeeService.java
@@ -0,0 +1,48 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.EmployeeDTO;
+import com.devsuperior.demo.entities.Department;
+import com.devsuperior.demo.entities.Employee;
+import com.devsuperior.demo.repositories.DepartmentRepository;
+import com.devsuperior.demo.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
+
+@Service
+public class EmployeeService implements Serializable {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    private void copyDTOToEntity(EmployeeDTO employeeDTO, Employee entity) {
+        entity.setName(employeeDTO.getName());
+        entity.setEmail(employeeDTO.getEmail());
+        Department department = departmentRepository.getReferenceById(employeeDTO.getDepartmentId());
+        entity.setDepartment(department);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<EmployeeDTO> findAllPaged(Pageable pageable) {
+        Pageable sortedByName = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("name"));
+        Page<Employee> employeeResult = employeeRepository.findAll(sortedByName);
+        return employeeResult.map(EmployeeDTO::new);
+    }
+
+    @Transactional
+    public EmployeeDTO insert(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        copyDTOToEntity(employeeDTO, employee);
+        employee = employeeRepository.save(employee);
+        return new EmployeeDTO(employee);
+    }
+}
