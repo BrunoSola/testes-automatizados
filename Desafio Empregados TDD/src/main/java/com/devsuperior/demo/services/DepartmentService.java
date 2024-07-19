dbc895ab2998fe48b36059b660c63edc4a46e6ad
BrunoSola <brunosola3@gmail.com>
2024-07-19 04:34:08 -0300

    feat: Implementa código conforme TDD DepartmentControllerIT

---
+++ Desafio Empregados TDD/src/main/java/com/devsuperior/demo/services/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.DepartmentDTO;
+import com.devsuperior.demo.entities.Department;
+import com.devsuperior.demo.repositories.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Transactional(readOnly = true)
+    public List<DepartmentDTO> findAll() {
+        List<Department> departments = departmentRepository.findAll(Sort.by("name"));
+        return departments.stream().map(DepartmentDTO::new).collect(Collectors.toList());
+    }
+}
